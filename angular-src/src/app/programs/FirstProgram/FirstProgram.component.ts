3ffcc1da252013cca6743788988b9bcaaf99c896
Phan Le <lephan031994@gmail.com>
2017-08-20 02:01:38 -0700

    design program page

--- angular-src/src/app/programs/FirstProgram/FirstProgram.component.ts
+++ angular-src/src/app/programs/FirstProgram/FirstProgram.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var jquery:any;
+declare var $ :any;
 @Component({
   selector: 'app-FirstProgram',
   templateUrl: './FirstProgram.component.html',
@@ -10,6 +12,7 @@ export class FirstProgramComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+     $('.materialboxed').materialbox();
   }
 
 }
