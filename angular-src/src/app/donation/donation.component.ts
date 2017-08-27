50ab42b59b28231579f5f7c04044466b0e85f228
Phan Le <lephan031994@gmail.com>
2017-08-27 11:45:26 -0700

    fixed donate button

--- angular-src/src/app/donation/donation.component.ts
+++ angular-src/src/app/donation/donation.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms";
+import { Http, Response, Headers } from '@angular/http';
+import { Observable } from 'rxjs';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-donation',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./donation.component.scss']
 })
 export class DonationComponent implements OnInit {
-
-  constructor() { }
+  constructor(private http: Http) { }
 
   ngOnInit() {
   }
+
+  onSubmit(form: NgForm) {
+    const body = "cmd=_s-xclick&hosted_button_id=MVHMXJLPQHZJE";
+    const headers = new Headers({"Content-Type": "application/x-www-form-urlencoded", "origin":"null"});
+    return this.http.post("https://www.paypal.com/cgi-bin/webscr", body, {headers: headers})
+           .map((response: Response) => {
+             const result = response.json();
+             return result;
+           })
+           .catch((error: Response) => Observable.throw(error.json())).subscribe(
+             data => {
+               console.log(data)
+             });
+    }
 
 }
