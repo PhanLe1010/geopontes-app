87e0049d60b310aa095926237429e721b818f1bd
Phan Le <PhanLe@berkeley.edu>
2019-03-13 15:42:08 -0700

    initial commit

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ var Schema  = mongoose.Schema;
 var schema = new Schema({
   email: {type: String, required: true, unique: true},
   password: {type: String, required:true},
-  fullName: {type: String, default: Date.now}
+  fullName: {type: String, required:true}
 })
 schema.plugin(mongooseUniqueValidator);
 module.exports = mongoose.model('User', schema);
